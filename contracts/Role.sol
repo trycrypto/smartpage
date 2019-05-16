91c8f717b0b12efce36acd796ef0ac9725f77581
niharrs <niharika.3297@gmail.com>
2019-05-16 11:51:48 +0530

    Created Role Module

---
+++ contracts/Role.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.5.1;
+
+contract Roles {
+    struct Role {
+        string name;
+    }
+    
+    address[] public roleAccounts;
+    
+    string[] allRoleNames;
+    
+    mapping (address => Role) roles;
+    
+    function setRole (address _address, string memory _name) public {
+        Role storage role = roles[_address];
+        role.name = _name;
+        
+        roleAccounts.push(_address);
+    }
+    
+    function getRoleAccounts() public view returns (address[] memory){
+        return roleAccounts;
+    }
+    
+    function getRole(address _address) public view returns (string memory){
+        return roles[_address].name;
+    }
+    
+    function deleteRole(address _address) public {
+        delete roles[_address];
+    }
+}
